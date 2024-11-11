4cd860a0c4efa6fd3a44668a92bf0a52ed56da37
Thabo Mokhutsoane <mokhutsoane@gmail.com>
2024-11-11 21:29:02 +0200

    update home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,21 +3,24 @@ import {
   IonHeader,
   IonToolbar,
   IonTitle,
-  IonContent,
-} from '@ionic/angular/standalone';
+  IonContent, IonButtons, IonButton, IonIcon, IonFab, IonFabButton, IonFabList, IonItem, IonList, IonLabel, IonCardSubtitle, IonCard, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logoIonic, heart, addOutline, addCircleSharp, add, chevronUpCircle, document, colorPalette, globe } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonCardSubtitle, IonLabel, IonList, IonItem, IonFabList, IonFabButton, IonFab, IonIcon, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage implements OnInit, OnDestroy {
-  constructor() {}
+  constructor() {
+    addIcons({add,chevronUpCircle,document,colorPalette,globe,addCircleSharp,addOutline,heart,logoIonic});
+  }
 
   myName: string = '';
-  ngOnDestroy(): void {}
+  ngOnDestroy(): void { }
   ngOnInit(): void {
     this.myName = 'Thabo';
   }
